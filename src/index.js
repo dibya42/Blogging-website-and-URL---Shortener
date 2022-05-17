910a6e86bd07f7944bb3bf3ad50993f32e428b59
ChanduKu <kuntrapakuc@gmail.com>
2022-05-17 12:45:21 +0530

    first version

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://chandu:root@newcluster.dek5n.mongodb.net/chandudb-project3", {
+mongoose.connect("mongodb+srv://chandu:root@newcluster.dek5n.mongodb.net/chandudb-project4", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
